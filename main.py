f81e262b32f2acfa78d0245656667ff24e1938ca
Seongbin Kim <sdbb2q@gmail.com>
2026-06-20 19:17:32 +0900

    random timestamp on logging and added serial reading function on main.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import model, os, serial
+from datetime import datetime
 import configparser as cfg
 
 cfg = cfg.ConfigParser()
@@ -7,21 +8,33 @@ cfg.read('./files/main_conf.ini', encoding='utf-8')
 class Main:
     def __init__(self):
         try:
-            print(f"smush starting, config loaded: {cfg.sections()}")
+            self.timestamp = datetime.now().strftime('%H:%M:%S')
+            print(f"[{self.timestamp}] [main] smush starting, config loaded: {cfg.sections()}")
             self.model = model.Model(cfg['GENERIC']['ModelPath'], cfg['GENERIC']['LabelsPath'])
             self.serial = serial.Serial(port=cfg['SERIAL']['SerialPort'], baudrate=cfg['SERIAL']['SerialBaudrate'], timeout=1)
             self.serial.open()
             if(not self.serial.is_open):
-                print("Failed to find serial port thats avaliable from configuration. Is the device connected?")
+                print(f"[{self.timestamp}] [main] Failed to find serial port thats avaliable from configuration. Is the device connected?")
                 return
-            print("Init done, waiting for serial.")
+            print(f"[{self.timestamp}] [main] Init done, waiting for serial..")
             self.serial_read()
         except:
-            print("Failed to init, check if all external components are available.")
+            print(f"[{self.timestamp}] [main] Failed to init, check if all external components are available.")
 
     def serial_read(self):
-        #todo: add serial connection.
-        pass
+        while True:
+            if self.serial.in_waiting > 0:
+                line = self.serial.readline().decode('utf-8').rstrip()
+                print(f"[{self.timestamp}] [main] read: {line}")
+
+                if line != "1": # invalid signal
+                    print(f"[{self.timestamp}] [main] read unknown signal.")
+                    return
+                
+                result = self.model.capture()
+                match result:
+                    case "": #todo: add model name
+                        pass
 
 if __name__ == "__main__":
     main = Main()
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from teachable_machine import TeachableMachine
 import cv2 as cv
 import configparser as config
@@ -5,18 +6,22 @@ import configparser as config
 class Model:
 	def __init__(self, model, labels):
 		try:
+			self.timestamp = datetime.now().strftime('%H:%M:%S')
 			self.vc = cv.VideoCapture(0)
 			self.model = TeachableMachine(model_path=model, labels_flie_path=labels)
 			self.path = "capture.png"
 			self.captured = False	
 			self.cfg = config.ConfigParser()
+			if(self.vc.isOpened() == False):
+				print(f"[{self.timestamp}] [ModelRecog] Failed to init camera. check if camera is available.")
+				quit()
 			try:
 				self.cfg.read('./files/model_conf.ini')
 			except FileNotFoundError:
-				print("Failed to read model configuration file. FileNotFoundException")
+				print(f"[{self.timestamp}] [ModelRecog] Failed to read model configuration file. FileNotFoundException")
 				quit()
 		except:
-			print("Failed to init model recog. Check if the camera and files are available.")
+			print(f"[{self.timestamp}] [ModelRecog] Failed to init model recog. Check if the files are available.")
 
 	def capture(self):
 		while self.captured == False:
@@ -25,10 +30,13 @@ class Model:
 			result = self.model.classify_image(self.path)
 			img_result = self.model.show_prediction_on_image(self.path, result)
 
-			print(f'class_index: {result["class_index"]}, confidence: {result["class_confidence"]}, predicted: {result["predictions"]}')
+			print(f"[{self.timestamp}] [ModelRecog] class_index: {result['class_index']}, confidence: {result['class_confidence']}, predicted: {result['predictions']}")
 			if self.cfg['GENERIC']['ShowCaptureVid'] == True:
 				cv.imshow("stream", img_result)
 				k = cv.waitKey(1)
 				if k == 27:
 					return result['predictions']
 			return result['predictions']
+		
+	def getIsitCaptured(self):
+		return self.captured
